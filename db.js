d4fb7c3a39af5a05021d218e139652ac005719ad
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2021-10-11 13:09:35 +0530

    set up mongodb and express server

---
+++ db.js
@@ -0,0 +1,28 @@
+// file to connect with basic mongodb server.
+
+const mongoose = require("mongoose");  // cjs used, not es6 modules
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+
+// mongoose returns promises which can be solved using then or async await.
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("Successfully connected to mongodb");
+    })
+}
+
+module.exports = connectToMongo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,23 @@
+// establishing connection to database
+const connectToMongo = require("./db");
+connectToMongo();
+
+// initializing express app
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Hardik!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
+
+
+
+
+
