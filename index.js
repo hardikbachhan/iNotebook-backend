c475b00d036173d9b543eff87f450ed14bbdef45
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-01-17 20:23:48 +0530

    setup middleware to read json from user

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// to use req.body
+app.use(express.json())
+
 // Available Routes
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/notes", require("./routes/notes"))
--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,8 @@ const express = require('express');
 const authRouter = express.Router()
 
 authRouter.get("/", (req, res) => {
-    const obj = {
-        name: "His si s",
-        number: 34
-    }
-    res.json(obj)
+    console.log(req.body);
+    res.send("Hello");
 })
 
 module.exports = authRouter
