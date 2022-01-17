00af0cdb11a93c3af84219495ac6844e992a9dcf
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-01-17 22:36:29 +0530

    connect user model to auth.js to add new user

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const authRouter = express.Router()
+const authRouter = express.Router();
+const User = require("../models/User")
 
-authRouter.get("/", (req, res) => {
+
+// Create a User using: POST "/api/auth/". Doesn't require Authentication.
+authRouter.post("/", (req, res) => {
     console.log(req.body);
-    res.send("Hello");
+    const user = User(req.body);
+    user.save();
+    res.send("user saved successfully");
 })
 
 module.exports = authRouter
