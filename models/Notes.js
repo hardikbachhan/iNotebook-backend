eb1e681ba19a892a75cbb27fb46794d92cbc35bc
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-01-20 15:17:50 +0530

    add foreign key in Notes.js

--- models/Notes.js
+++ models/Notes.js
@@ -5,6 +5,10 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+  user: {       // foreign key
+    type: mongoose.Schema.Types.ObjectId,   // objectid of a document of another model 
+    ref: 'user'                             // model in which above id is present
+  },
   title: {
     type: String,
     required: true
