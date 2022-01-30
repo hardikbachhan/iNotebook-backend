9b9a1125f39a793134c099fbab60df8f9017facc
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-01-30 15:35:39 +0530

    add cors middleware in index.js

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ connectToMongo();
 
 // initializing express app
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
 // to use req.body
 app.use(express.json())
 
+// to enable cors
+app.use(cors());
+
 // Available Routes
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/notes", require("./routes/notes"))
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`iNotebook backend listening at http://localhost:${port}`);
 });
 
